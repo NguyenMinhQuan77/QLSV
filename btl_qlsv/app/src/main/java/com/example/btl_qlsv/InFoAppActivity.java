06bf36e9975e080881172d010ac7a6fcfbcd6d14
NguyenMinhQuan77 <quan772k32003@gmail.com>
2024-05-10 16:41:23 +0700

    QLSV

---
+++ btl_qlsv/app/src/main/java/com/example/btl_qlsv/InFoAppActivity.java
@@ -0,0 +1,14 @@
+package com.example.btl_qlsv;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InFoAppActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_info_app);
+    }
+}
